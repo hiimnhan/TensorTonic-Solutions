8bdaeacd921f5fc121936a5841aa9fa9bb4aa197
Nhan Nguyen <35358825+hiimnhan@users.noreply.github.com>
2026-08-05 16:37:09 +0100

    Solve gae-computation

--- gae-computation/gae-computation.py
+++ gae-computation/gae-computation.py
@@ -4,13 +4,11 @@ def gae(rewards, values, gamma, lam):
     """
     T = len(rewards)
     advantages = [0.0] * T
-    last_adv = 0.0
-
-    for t in range(T - 1, -1, -1):
+    running = 0.0
+    for t in reversed(range(T)):
         delta = rewards[t] + gamma * values[t + 1] - values[t]
-        advantages[t] = delta + gamma * lam * last_adv
-        last_adv = advantages[t]
+        advantages[t] = delta + gamma * lam * running
+        running = advantages[t]
 
     return advantages
-        
         
